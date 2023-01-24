14a3fedd068db0c205f29f76d27d66c7c2efdf2e
UP210305 <up210305@alumnos.upa.edu.mx>
2023-01-23 23:52:54 -0600

    C1

---
+++ U1/02_SecretNumber.py
@@ -0,0 +1,12 @@
+import random as random
+
+secret = random.randint(1, 20)
+
+number = int(input("Adivina el numero secreto: "))
+
+while number != secret:
+    print("Lo siento, intentalo devuelta")
+    number = int(input("Ingresa un numero diferente: "))
+else:
+    print("Correcto, haz acertado el numero!!!")
+
