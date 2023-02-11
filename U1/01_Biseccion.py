71660b6bedef77dcc505f0f472b8b305fc41a94a
UP210305 <up210305@alumnos.upa.edu.mx>
2023-02-10 19:16:47 -0600

    Modified

--- U1/01_Biseccion.py
+++ U1/01_Biseccion.py
@@ -1,48 +1,4 @@
-'''
-#include <math.h>    // math.h
-#include <iomanip>  // setprecision(), setw()
-#include <iostream>
-using namespace std;
 
-double fnEcuacion(double valor) { return pow(valor, 2) - 2; }
-
-int main(int argc, char const *argv[]) {
-    double x1 = 1;
-    double x2 = 2;
-    double xm;
-    double Es = 0.001;          // Error Standard o Absoluto
-    double Er = abs(x2-x1);     // Error Relativo
-    int i = 1;
-    double it = round((log(x2 - x1) - log(Es)) / log(2));
-    cout << it << endl;
-    /*
-    cout << "i" << "\t" << "x1" << "\t" << "x2" <<"\t" << "Er" <<"\t" << "xm"
-         << "\t" << "fnEc(x1)" << "\t" << "fnEc(xm)" << endl;
-    */     
-    printf("i   |     x1    |     x2    |      Er   |     xm    |" \
-           "   f(x1)   |   f(xm)   | f(x1) * f(xm) |\n");
-    while (Er >= Es) {
-        xm = (x1 + x2) / 2;
-        /*
-        cout << fixed << left << setprecision(4) << i << "\t" << x1 << "\t"
-             << x2 << "\t" << Er << "\t" << xm << "\t" << fnEcuacion(x1) << "\t"
-             << fnEcuacion(xm) << endl;
-        */   
-       printf("%3d |%10.4f |%10.4f |%10.4f |%10.4f |%10.4f |%10.4f \n",
-               i, x1, x2, Er, xm,  fnEcuacion(x1), fnEcuacion(xm));
-        if (fnEcuacion(x1) * fnEcuacion(xm) < 0) {
-            x2 = xm;
-        } else {
-            x1 = xm;
-        }
-        Er = abs(x2 - x1);
-        i = i + 1;
-    }
-    cout << xm << endl;
-    return 0;
-}
-'''
-import math as math
 import numpy as np
 
 def fnEcuacion(valor):
@@ -56,19 +12,20 @@ Er = abs(x2-x1)
 i = 1
 it = round((np.log(x2 - x1) - np.log(Es)) / np.log(2))
 
-print(it)
-print("i\t|\tx1\t|\tx2\t|\tEr\t|\txm|\t" \
-           "f(x1)\t|\tf(xm)\t|\tf(x1) * f(xm)\t|\n")
+print("Iterations: ", it)
+print("i\t|x1\t|x2\t|er\t|xm\t|f(x1)\t|f(xm)\t|f(x1)*f(xm)")
 
 while (Er >= Es):
+
     xm = (x1 + x2) / 2
-    print( i ,"\t|\t", x1 ,"\t|\t", x2 ,"\t|\t", Er ,"\t|\t",xm  ,"\t|\t", fnEcuacion(x1), "\t|\t", fnEcuacion(xm), "\n")
+    print(i,round(x1, 3), round(x2, 3), round(Er, 3), round(xm, 3), round(fnEcuacion(x1), 3), round(fnEcuacion(xm), 3), round((fnEcuacion(x1) * fnEcuacion(xm)),3), sep="\t|")
+    
     if (fnEcuacion(x1) * fnEcuacion(xm) < 0):
-            x2 = xm
+        x2 = xm
     else :
         x1 = xm
     
-        Er = abs(x2 - x1)
-        i = i + 1;        
+    Er = abs(x2 - x1)
+    i += 1        
 
-print(xm, "\n")
+print("\nRoot: ", xm, "\n")
